ebf09f8622fea6e864019855b91c7850c8c64deb
faitti <aleksanteri.nissinen@jyno.fi>
2022-12-29 20:14:42 +0200

    theme toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,48 @@
-import { NavbarElement } from "./components/navbar";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import NavbarElement from "./components/Navbar";
+import { themeContext } from "./Theme";
 
-function App() {
-  return (
-    <>
-      <NavbarElement />
-    </>
-  );
+export type ThemeState = {
+  theme: string;
+};
+
+export class App extends React.Component<{}, ThemeState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      theme: "light",
+    };
+  }
+
+  toggleTheme = () => {
+    this.setState((state: ThemeState) => ({
+      theme: state.theme === "dark" ? "light" : "dark",
+    }));
+  };
+
+  render() {
+    document.documentElement.style.setProperty(
+      "--bodyColor",
+      this.state.theme == "dark" ? "#101010" : "#ffffff"
+    );
+    return (
+      <>
+        <themeContext.Provider value={this.state.theme}>
+          <NavbarElement
+            theme={this.state.theme}
+            toggleTheme={this.toggleTheme}
+          />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </themeContext.Provider>
+      </>
+    );
+  }
 }
+
+App.contextType = themeContext;
 
 export default App;
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,18 +1,51 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Button, Nav, Navbar, ToggleButton } from "react-bootstrap";
+import { themeContext } from "../Theme";
 import Container from "react-bootstrap/esm/Container";
+import { WiDaySunny, WiStars } from "react-icons/wi";
+import React from "react";
 
-export const NavbarElement = () => {
-  return (
-    <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="">Urho</Navbar.Brand>
-          <Nav className="justify-content-end" style={{ width: "100%" }}>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#register">Register</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
+type PropsType = {
+  theme: string;
+  toggleTheme: () => void;
 };
+
+class NavbarElement extends React.Component<PropsType, {}> {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return (
+      <>
+        <Navbar
+          bg={theme === "dark" ? "light" : "dark"}
+          variant={theme === "dark" ? "light" : "dark"}
+        >
+          <Container>
+            <Navbar.Brand href="">Urho</Navbar.Brand>
+            <Nav className="justify-content-end" style={{ width: "100%" }}>
+              <Nav.Link href="login">Login</Nav.Link>
+              <Nav.Link href="register">Register</Nav.Link>
+              <Button
+                onClick={props.toggleTheme}
+                style={{
+                  backgroundColor: "transparent",
+                  borderColor: "transparent",
+                  color: theme === "dark" ? "#101010" : "#ffffff",
+                }}
+              >
+                {theme == "dark" ? (
+                  <WiDaySunny size="30" />
+                ) : (
+                  <WiStars size="30" />
+                )}
+              </Button>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+}
+
+NavbarElement.contextType = themeContext;
+
+export default NavbarElement;
